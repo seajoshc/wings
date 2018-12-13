6443f859a7f8c2d9b36ab404eecf1f3d2768b0ef
Josh Campbell <josh@userdel.com>
2018-12-13 18:42:31 -0500

    initial commit

---
+++ wings-runner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+Convenience wrapper for running wings directly from source tree.
+"""
+from wings.wings import main
+
+
+if __name__ == '__main__':
+    main()
---
+++ wings/__main__.py
@@ -0,0 +1,7 @@
+"""
+wings.__main__: executed when wings directory is called as script
+"""
+from .wings import main
+
+
+main()
