0dfad3fa10e0c2238e3ef9824d66b44872b34b94
Josh Campbell <josh@userdel.com>
2018-12-13 13:56:09 -0500

    boilerplate

---
+++ wings/wings.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+wings
+Dead simple CI/CD for your AWS projects.
+"""
+import argparse
+import boto3
+
+
+def main():
+    """ """
+    return
+
+
+if __name__ == "__main__":
+    """ Your favorite wrapper's favorite wrapper """
+    main()
