41c111cf2cfb220c0fd83dbffb7bcc287847fb36
Josh Campbell <josh@userdel.com>
2018-12-13 19:05:16 -0500

    changes exceptions to better reflect the error

--- wings/validate_config.py
+++ wings/validate_config.py
@@ -15,18 +15,18 @@ class ValidateConfig():
 
         for key in REQUIRED_KEYS:
             if key not in self.config.keys():
-                raise IOError(
+                raise KeyError(
                     "Required key '{}' missing from wings.toml.".format(key))
 
         if 'service' not in self.config['runtime']:
-            raise IOError("Required key 'service' missing from [runtime].")
+            raise KeyError("Required key 'service' missing from [runtime].")
 
         # Next, we make check for a valid runtime service.
         RUNTIME_SERVICES = ["lambda"]
         service = self.config['runtime']['service']
 
         if service not in RUNTIME_SERVICES:
-            raise IOError(
+            raise ValueError(
                 "'{}' is an invalid runtime service. Must be one of: {}"
                 .format(service, ', '.join(RUNTIME_SERVICES)))
 
@@ -34,13 +34,13 @@ class ValidateConfig():
         LAMBDA_LANGUAGES = ["python36"]
         if service == 'lambda':
             if 'language' not in self.config['runtime']:
-                raise IOError(
+                raise KeyError(
                     "Required key 'language' missing from [runtime]. "
                     "Supported values for 'language' are: "
                     "{}".format(', '.join(LAMBDA_LANGUAGES)))
 
             language = self.config['runtime']['language']
             if language not in LAMBDA_LANGUAGES:
-                raise IOError("'{}' is an invalid lanaguage for Lambda. "
+                raise ValueError("'{}' is an invalid lanaguage for Lambda. "
                               "Supported languages are: {}".format(
                                 language, ', '.join(LAMBDA_LANGUAGES)))
