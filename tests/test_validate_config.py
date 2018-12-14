220f74dbe25cf753ca83d7260076bd982ac0e34e
Josh Campbell <josh@userdel.com>
2018-12-13 21:00:44 -0500

    add tests for 100% coverage

--- tests/test_validate_config.py
+++ tests/test_validate_config.py
@@ -39,3 +39,75 @@ def test_validate_config_with_missing_name_key(mock_toml_load):
 
     with pytest.raises(KeyError):
         ValidateConfig('/fake_path_yo/wings.toml').config
+
+
+@patch('toml.load')
+def test_validate_config_with_missing_runtime_key(mock_toml_load):
+    mock_toml_load.return_value = toml.loads("""
+        name = "wings"
+        description = "Weeeee"
+
+        service = "lambda"
+        language = "python36"
+    """)
+
+    with pytest.raises(KeyError):
+        ValidateConfig('/fake_path_yo/wings.toml').config
+
+
+@patch('toml.load')
+def test_validate_config_with_missing_service_key(mock_toml_load):
+    mock_toml_load.return_value = toml.loads("""
+        name = "wings"
+        description = "Weeeee"
+
+        [runtime]
+        language = "python36"
+    """)
+
+    with pytest.raises(KeyError):
+        ValidateConfig('/fake_path_yo/wings.toml').config
+
+
+@patch('toml.load')
+def test_validate_config_with_invalid_service_value(mock_toml_load):
+    mock_toml_load.return_value = toml.loads("""
+        name = "wings"
+        description = "Weeeee"
+
+        [runtime]
+        service = "blah"
+        language = "python36"
+    """)
+
+    with pytest.raises(ValueError):
+        ValidateConfig('/fake_path_yo/wings.toml').config
+
+
+@patch('toml.load')
+def test_validate_config_with_missing_language_key(mock_toml_load):
+    mock_toml_load.return_value = toml.loads("""
+        name = "wings"
+        description = "Weeeee"
+
+        [runtime]
+        service = "lambda"
+    """)
+
+    with pytest.raises(KeyError):
+        ValidateConfig('/fake_path_yo/wings.toml').config
+
+
+@patch('toml.load')
+def test_validate_config_with_invalid_language(mock_toml_load):
+    mock_toml_load.return_value = toml.loads("""
+        name = "wings"
+        description = "Weeeee"
+
+        [runtime]
+        service = "lambda"
+        language = "python69420"
+    """)
+
+    with pytest.raises(ValueError):
+        ValidateConfig('/fake_path_yo/wings.toml').config
