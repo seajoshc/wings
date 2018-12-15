ea134f392a325a51dab17152367b22e7ba395dc9
Josh Campbell <josh@userdel.com>
2018-12-15 13:17:31 -0500

    starts setting up argparse

--- wings/wings.py
+++ wings/wings.py
@@ -5,14 +5,34 @@ Dead simple CI/CD for your AWS projects.
 """
 import argparse
 import os
-import boto3
-from .validate_config import ValidateConfig
+from wings.__version__ import __version__
+from wings.validate_config import ValidateConfig
 
 
 def main():
     """ """
-    config = ValidateConfig(os.getcwd() + "/wings.toml").config
-    print(config)
+    args = parse_args()
+
+    if args.up:
+        config = ValidateConfig(os.getcwd() + "/wings.toml").config
+        print(config)
+
+
+def parse_args():
+    """ Setup argument parser """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("up",
+                        help="Reads a wings.toml file to create or update "
+                             "CI/CD toolchains.",
+                        type=str)
+
+    parser.add_argument("-v", "--version",
+                        action='version',
+                        help="Prints the version of wings you are using.",
+                        version="wings {}".format(__version__))
+
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
