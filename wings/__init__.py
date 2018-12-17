34138c03bea093ca931a07fe91a038508b4aa528
Josh Campbell <josh@userdel.com>
2018-12-16 21:11:57 -0500

    adds Toolchain

--- wings/__init__.py
+++ wings/__init__.py
@@ -1,8 +1,10 @@
-from .wings import ValidateConfig
+from wings.wings import ValidateConfig
+from wings.aws import Toolchain
 
 __author__ = "Josh Campbell"
 __license__ = "MIT"
 
 __all__ = [
     "ValidateConfig",
+    "Toolchain",
 ]
